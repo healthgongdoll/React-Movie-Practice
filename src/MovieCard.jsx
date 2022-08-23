c7cf290fab05486d5a17ed9323e4afa1d67c3a40
Jae Young Chung <wayne961025@gmail.com>
2022-08-22 23:18:06 -0400

    React JS Practice

---
+++ src/MovieCard.jsx
@@ -0,0 +1,23 @@
+//React jsx is referable 
+
+import React from 'react';
+
+const movieCard = ({ movie }) => {
+    return (
+    <div className="movie">
+    <div>
+    <p>{movie.Year}</p>
+    </div>
+
+    <div>
+    <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/404'} alt={movie.Title}/>
+    </div>
+
+    <div>
+        <span>{movie.Type}</span>
+        <h3>{movie.Title}</h3>
+    </div>
+</div>);
+}
+
+export default movieCard;
